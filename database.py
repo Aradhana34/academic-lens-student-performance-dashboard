448a66bb02de3d408426ba7a74860627c44a2f5b
Aradhana34 <daradhana240@gmail.com>
2026-09-20 13:29:54 +0530

    Fix database

--- database.py
+++ database.py
@@ -5,21 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_db_connection():
-
-    connection = mysql.connector.connect(
+    return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-
-        ssl_ca=os.path.join(
-            os.path.dirname(__file__),
-            "ca.pem"
-        ),
-
-        ssl_verify_cert=True,
-        ssl_verify_identity=True
+        ssl_disabled=False
     )
-
-    return connection
